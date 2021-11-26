5eedd6db9aa95eb233104cfa3ed0290f93345c5c
Darko Markan <75615049+dmarkan@users.noreply.github.com>
2021-11-26 16:58:18 +0100

    added styling

--- app.js
+++ app.js
@@ -3,13 +3,11 @@ const bodyParser = require("body-parser");
 const app = express();
 const https = require("https");
 
-
-
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
-
 })
 
 app.post("/", function(req,res) {
@@ -23,16 +21,16 @@ app.post("/", function(req,res) {
             const storyTitle = storyData.title;
             const storyAuthor = storyData.author;
             const storyText = storyData.story;
-            res.write("<h1>" + storyTitle + "</h1>");
-            res.write("<h3>by " + storyAuthor + "</h3>");
-            res.write("<p>" + storyText + "</p>");
+            res.write("<body style='background-image: url(images/background2.jpg); background-size: cover; background-attachment: fixed; background-repeat: no-repeat;'>")
+            res.write("<h1 style='text-align:center;color:#fff;font-size:5rem;'>" + storyTitle + "</h1>");
+            res.write("<h3 style='text-align:center;color:#fff;;font-size:1.5rem;font-style: italic;'>by " + storyAuthor + "</h3>");
+            res.write("<p style='text-align:center;color:#fff;;font-size:0.7rem;margin:0 1rem;'>" + storyText + "</p>");
             res.write("<script>let msg = new SpeechSynthesisUtterance();msg.text = '" + storyText + "';msg.rate = 0.6;window.speechSynthesis.speak(msg); </script>")
-            res.send();
-          
+            res.send();        
         })
     })
 })
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log("Server is running on port 3000");
 })
